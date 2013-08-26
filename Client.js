621e25f14c87b801916a9abf226719fc10eab66d
derpthebass <ramboxp@live.com>
2013-08-26 17:06:08 -0400

    Update Client.js

--- Client.js
+++ Client.js
@@ -3,18 +3,17 @@ var client = $('<link>');
 client.attr("href", "http://cdn.socket.io/stable/socket.io.js")
 $("head").append(client);
 
-	var socket = io.connect('http://playmc.pw:1337');
+    var socket = io.connect('<your domain/ip:port you used for the server');
 
 smsNotifications = true;
 API.on(API.CHAT_COMMAND, function(data){
-	if(data == "/sms") smsNotifications = !smsNotifications;
+    if(data == "/sms") smsNotifications = !smsNotifications;
 });
 
 socket.on('connect', function(){
-		API.on(API.CHAT, function(data){
-		if(data.type == "mention" && smsNotifications === true){
- 		msg = data.message.replace(/&#39;/g, "'");
-		socket.emit('smsMention', data.from, msg);
-		}		
-	});
+        API.on(API.CHAT, function(data){
+        if(data.type == "mention" && smsNotifications === true){
+        socket.emit('smsMention', data.from, data.message);
+        }       
+    });
 });
